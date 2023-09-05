dcf6dde889cde9998a2c969a88e7f9f9f8823f5e
Jeszpy <hleb.lukashonak@gmail.com>
2023-09-06 00:12:45 +0300

    feat: add utilities for e2e tests

---
+++ test/endpoints.ts
@@ -0,0 +1,6 @@
+export const endpoints = {
+  users: '/users',
+  testing: {
+    wipeAllData: '/testing/wipe-all',
+  },
+};
---
+++ test/testing-factories/users-testing.factory.ts
@@ -0,0 +1,11 @@
+import { CreateUserDto } from '../../src/modules/users/dto/create-user.dto';
+import request from 'supertest';
+import { endpoints } from '../endpoints';
+
+export class UsersTestingFactory {
+  constructor(private readonly server: any) {}
+
+  public async createUser(dto: CreateUserDto): Promise<request.Response> {
+    return request(this.server).post(endpoints.users).send(dto);
+  }
+}
