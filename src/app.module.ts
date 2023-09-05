4c7ca88d4b0def55b0a2465715c79fb3178f18df
Jeszpy <hleb.lukashonak@gmail.com>
2023-09-05 23:49:55 +0300

    feat: inject testing module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModuleOptions } from './options/config-module.options';
 import { TypeOrmOptions } from './options/type-orm-module.options';
 import { UsersModule } from './modules/users/users.module';
+import { TestingModule } from './modules/testing/testing.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(ConfigModuleOptions()),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmOptions }),
     UsersModule,
+    TestingModule,
   ],
 })
 export class AppModule {}
