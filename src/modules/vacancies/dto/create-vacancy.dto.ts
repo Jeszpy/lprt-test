a1b271ff2bc2bea1d0a329cd115154662ca37980
Jeszpy <hleb.lukashonak@gmail.com>
2023-09-07 00:56:38 +0300

    feat: create dto's for vacancy

--- src/modules/vacancies/dto/create-vacancy.dto.ts
+++ src/modules/vacancies/dto/create-vacancy.dto.ts
@@ -1,1 +1,27 @@
-export class CreateVacancyDto {}
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsString,
+  IsUUID,
+  Length,
+} from 'class-validator';
+import { IsPublisherExists } from '../../../decorators/validation/is-publisher-exists.decorator';
+
+export class CreateVacancyDto {
+  @Length(5, 30)
+  @IsString()
+  title: string;
+
+  @Length(25, 300)
+  @IsString()
+  shortDescription: string;
+
+  @IsString({ each: true })
+  @ArrayNotEmpty()
+  @IsArray()
+  skills: string[];
+
+  @IsPublisherExists()
+  @IsUUID()
+  publisherId: string;
+}
--- src/modules/vacancies/dto/update-vacancy.dto.ts
+++ src/modules/vacancies/dto/update-vacancy.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateVacancyDto } from './create-vacancy.dto';
+import { ArrayNotEmpty, IsArray, IsString, Length } from 'class-validator';
 
-export class UpdateVacancyDto extends PartialType(CreateVacancyDto) {}
+export class UpdateVacancyDto {
+  @Length(5, 30)
+  @IsString()
+  title: string;
+
+  @Length(25, 300)
+  @IsString()
+  shortDescription: string;
+
+  @IsString({ each: true })
+  @ArrayNotEmpty()
+  @IsArray()
+  skills: string[];
+}
