b1f9282345e2406f3e21d613c9d168dd911a46bb
Jeszpy <hleb.lukashonak@gmail.com>
2023-09-12 23:41:10 +0300

    feat: add new enum

---
+++ src/enums/pagination.enums.ts
@@ -0,0 +1,11 @@
+export enum SortDirectionEnum {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+export enum VacancySortByEnum {
+  CREATED_AT = 'createdAt',
+  TITLE = 'title',
+  USER_NAME = 'userName',
+  SKILLS = 'skills',
+}
