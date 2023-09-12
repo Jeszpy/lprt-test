53cf687da1fc65eb5d602f1eb353d7548d312ee3
Jeszpy <hleb.lukashonak@gmail.com>
2023-09-12 23:42:12 +0300

    feat: create PUT and GET method logic for vacancies

--- src/modules/vacancies/vacancies.controller.ts
+++ src/modules/vacancies/vacancies.controller.ts
@@ -8,10 +8,14 @@ import {
   ParseUUIDPipe,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import { VacanciesService } from './vacancies.service';
 import { CreateVacancyDto } from './dto/create-vacancy.dto';
 import { UpdateVacancyDto } from './dto/update-vacancy.dto';
+import { IVacancyViewModel, Vacancy } from './entities/vacancy.entity';
+import { VacanciesPagination } from '../../helpers/pagination/vacancies.pagination';
+import { TPagination } from '../../helpers/pagination/default.pagination';
 
 @Controller('vacancies')
 export class VacanciesController {
@@ -19,26 +23,30 @@ export class VacanciesController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  create(@Body() createVacancyDto: CreateVacancyDto) {
+  public async create(
+    @Body() createVacancyDto: CreateVacancyDto,
+  ): Promise<IVacancyViewModel> {
     return this.vacanciesService.createVacancy(createVacancyDto);
   }
 
   @Get()
   @HttpCode(HttpStatus.OK)
-  findAll() {
-    return this.vacanciesService.getAllVacancies();
+  public async findAll(
+    @Query() pagination: VacanciesPagination,
+  ): Promise<TPagination<Vacancy>> {
+    return this.vacanciesService.getAllVacancies(pagination);
   }
 
   @Put(':id')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  update(
+  @HttpCode(HttpStatus.OK)
+  public async update(
     @Param(
       'id',
       new ParseUUIDPipe({ errorHttpStatusCode: HttpStatus.NOT_FOUND }),
     )
     id: string,
     @Body() updateVacancyDto: UpdateVacancyDto,
-  ) {
-    return this.vacanciesService.updateVacancyById(+id, updateVacancyDto);
+  ): Promise<IVacancyViewModel> {
+    return this.vacanciesService.updateVacancyById(id, updateVacancyDto);
   }
 }
--- src/modules/vacancies/vacancies.service.ts
+++ src/modules/vacancies/vacancies.service.ts
@@ -1,10 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateVacancyDto } from './dto/create-vacancy.dto';
 import { UpdateVacancyDto } from './dto/update-vacancy.dto';
 import { Repository } from 'typeorm';
-import { Vacancy, IVacancyCreateAttr } from './entities/vacancy.entity';
+import {
+  IVacancyCreateAttr,
+  IVacancyViewModel,
+  Vacancy,
+} from './entities/vacancy.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { VacanciesPagination } from '../../helpers/pagination/vacancies.pagination';
+import { FindOptionsOrder } from 'typeorm/find-options/FindOptionsOrder';
+import { TPagination } from '../../helpers/pagination/default.pagination';
+import { VacancySortByEnum } from '../../enums/pagination.enums';
 
 @Injectable()
 export class VacanciesService {
@@ -14,6 +22,20 @@ export class VacanciesService {
     @InjectRepository(User)
     private readonly userModel: Repository<User>,
   ) {}
+
+  private getOrderBy(
+    pagination: VacanciesPagination,
+  ): FindOptionsOrder<Vacancy> {
+    if (
+      pagination.sortBy.toUpperCase() ===
+      VacancySortByEnum.USER_NAME.toUpperCase()
+    ) {
+      return { publisher: { userName: pagination.sortDirection } };
+    } else {
+      return { [`${pagination.sortBy}`]: pagination.sortDirection };
+    }
+  }
+
   public async createVacancy(createVacancyDto: CreateVacancyDto) {
     const publisher = await this.userModel.findOneBy({
       id: createVacancyDto.publisherId,
@@ -29,14 +51,34 @@ export class VacanciesService {
     return newVacancy.getViewModel();
   }
 
-  public async getAllVacancies(): Promise<Vacancy[]> {
-    return this.vacancyModel.find();
+  public async getAllVacancies(
+    pagination: VacanciesPagination,
+  ): Promise<TPagination<Vacancy>> {
+    const [vacancies, count] = await this.vacancyModel.findAndCount({
+      order: this.getOrderBy(pagination),
+      skip: pagination.skip(),
+      take: pagination.pageSize,
+    });
+    const pagesCount = Math.ceil(count / pagination.pageSize);
+    return {
+      pageSize: pagination.pageSize,
+      page: pagination.pageNumber,
+      pagesCount: pagesCount === 0 ? 1 : pagesCount,
+      totalCount: count,
+      items: vacancies,
+    };
   }
 
   public async updateVacancyById(
-    id: number,
+    id: string,
     updateVacancyDto: UpdateVacancyDto,
-  ) {
-    return `This action updates a #${id} vacancy`;
+  ): Promise<IVacancyViewModel> {
+    const vacancy = await this.vacancyModel.findOneBy({ id });
+    if (!vacancy) throw new NotFoundException();
+    vacancy.title = updateVacancyDto.title;
+    vacancy.shortDescription = updateVacancyDto.shortDescription;
+    vacancy.skills = updateVacancyDto.skills;
+    const updatedVacancy = await this.vacancyModel.save(vacancy);
+    return updatedVacancy.getViewModel();
   }
 }
